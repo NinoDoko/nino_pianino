8a44bf15d883e947090e0dcf3992a82d3c9fc54a
NinoDoko <ninodokoskiot@hotmail.com>
2016-04-13 15:01:59 +0200

    Bunch of changes - manily, added a key model for better note generating

--- generator.py
+++ generator.py
@@ -1,44 +1,55 @@
 from midiutil.MidiFile import MIDIFile
 import music_models, argparse, random
 
+def gen_notes_for_key(number_notes, key = 'A', scale = 'minor', time = 0, duration = 1):
+    k = music_models.Key(key, scale)
+    prev_note = random.choice(k.notes)
+    notes = []
+    while number_notes>0:
+        notes.append(music_models.Note(note = prev_note, time = time, duration = duration, volume = 100)) 
+        prev_note = k.generate_note(prev_note, 7)
+        number_notes -= 1 
+    return notes
+
+
 def gen_note(prev_note = None, t=0, d=1, diffs = []):
-  if prev_note:
-    if not diffs : 
-        diffs = [i for i in [-12, -11, -9, -7, -5, -4, -2, 0, 2, 4, 5, 7, 9, 11, 12] if prev_note.pitch-21-i > 0 and i+prev_note.pitch-21 <len(music_models.notes_list)-1]
-    r_note = music_models.notes_list[prev_note.pitch - 21 + random.choice(diffs)]
-  else:
-    r_note=random.choice(music_models.notes_list)
-  print r_note
-  return music_models.Note(note = r_note, time = t, duration = d, volume = 100)
+    if prev_note:
+        if not diffs : 
+            diffs = [i for i in [-12, -11, -9, -7, -5, -4, -2, 0, 2, 4, 5, 7, 9, 11, 12] if prev_note.pitch-21-i > 0 and i+prev_note.pitch-21 <len(music_models.notes_list)-1]
+        r_note = music_models.notes_list[prev_note.pitch - 21 + random.choice(diffs)]
+    else:
+        r_note=random.choice(music_models.notes_list)
+    print r_note
+    return music_models.Note(note = r_note, time = t, duration = d, volume = 100)
 
 def main():
 
-  parser = argparse.ArgumentParser(description="Basic arguments")
-  parser.add_argument('--some_argument', help = 'Stub argument', default = '')
-  parser.add_argument('--number_notes', help = 'Number of notes to play ', default = 50, type = int)
-  parser.add_argument('--notes_duration_range_min', help = 'Top range for variation of note duration. ', type = int, default = 0.5)
-  parser.add_argument('--notes_duration_range_max', help = 'Lower range for variation of note duration. ', type = int, default = 2)
-  parser.add_argument('--output', help = 'Path for output midi file. ', default = 'output.mid')
+    parser = argparse.ArgumentParser(description="Basic arguments")
+    parser.add_argument('--some_argument', help = 'Stub argument', default = '')
+    parser.add_argument('--number_notes', help = 'Number of notes to play ', default = 50, type = int)
+    parser.add_argument('--notes_duration_range_min', help = 'Top range for variation of note duration. ', type = int, default = 0.5)
+    parser.add_argument('--notes_duration_range_max', help = 'Lower range for variation of note duration. ', type = int, default = 2)
+    parser.add_argument('--output', help = 'Path for output midi file. ', default = 'output.mid')
 
-  args = parser.parse_args()
-  n_notes = args.number_notes
-  duration_max = args.notes_duration_range_max
-  duration_min = args.notes_duration_range_min
-  output = args.output
+    args = parser.parse_args()
+    n_notes = args.number_notes
+    duration_max = args.notes_duration_range_max
+    duration_min = args.notes_duration_range_min
+    output = args.output
 
-  mid = MIDIFile(1)
-  mid.addTrackName(0, 0, 'Sample')
-  prev_note = gen_note()
-  t = 0
+    mid = MIDIFile(1)
+    mid.addTrackName(0, 0, 'Sample')
+#    prev_note = gen_note()
+    t = 0
+    gen_notes_for_key(50)
+#    while n_notes:
+#        mid.addNote(*prev_note.get_values())
+#        prev_note = gen_note(prev_note=prev_note, t=t, d=1)
+#        n_notes -= 1
+#        t += 1
 
-  while n_notes:
-    mid.addNote(*prev_note.get_values())
-    prev_note = gen_note(prev_note=prev_note, t=t, d=1)
-    n_notes -= 1
-    t += 1
-  
-  binfile = open(output, 'wb')
-  mid.writeFile(binfile)
-  binfile.close()
+    binfile = open(output, 'wb')
+    mid.writeFile(binfile)
+    binfile.close()
 
 main()
--- music_models.py
+++ music_models.py
@@ -1,16 +1,48 @@
-import itertools
+import itertools, random
 
-notes_list = [x + y for y in  [str(i) for i in range(8)] for x in ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']]
+base_notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+notes_list = [x + y for y in  [str(i) for i in range(8)] for x in base_notes]
 
+#Note class; mostly used to add data to a midi file. 
 class Note:
-  def __init__(self, track = 0, channel = 0, note = 'A0', time = 0, duration = 0, volume = 0):
-    self.track = track
-    self.channel = channel
-    self.pitch = notes_list.index(note) + 21
-    self.time = time
-    self.duration = duration
-    self.volume = volume
+    def __init__(self, track = 0, channel = 0, note = 'A0', time = 0, duration = 0, volume = 0):
+        self.track = track
+        self.channel = channel
+        self.pitch = notes_list.index(note) + 21
+        self.time = time
+        self.duration = duration
+        self.volume = volume
 
-  def get_values(self):
-    return self.track, self.channel, self.pitch, self.time, self.duration, self.volume
+    def get_values(self):
+        return self.track, self.channel, self.pitch, self.time, self.duration, self.volume
 
+#The key class offers functionality relating to generating random notes based off of some rules. 
+#The key argument is what the root note of the key is, for instance, the note A. This argument can be ommited if using the base_notes argument. 
+#The scale is the name of the scale that the other notes will be generated off. For instance, if using a minor scale with the A as the root of the key, if will generate all the notes belonging to the A minor scale. This argument can be ommited if using the base_notes argument. 
+#If you wish to define your own notes that define the key (maybe I missed one or you just want to use a group of notes), you can supply a list of base notes that the key will use (example : ['A', 'C', 'D', 'D#', G#']). This list should not contain flat notes (yes, I know there's a reason for flat and sharp notes existing, but for the sake of code simplicity, I just use sharp notes). 
+
+class Key:
+    keys_diffs = {
+        'major' : [0, 2, 4, 5, 7, 9, 11],
+        'minor' : [0, 2, 3, 5, 7, 8, 10],
+    }
+    
+    def __init__(self, key = 'A', scale = 'minor', b_notes = []):
+        self.key = key
+        self.diffs = [(x + base_notes.index(key))%len(base_notes) for x in self.keys_diffs[scale]]
+        if not b_notes : 
+            self.base_notes = [base_notes[x] for x in self.diffs]
+        else : 
+            self.base_notes = base_notes
+        self.notes = [x for x in notes_list if x[:-1] in self.base_notes]
+        
+    def generate_note(self, note_pivot, note_radius = 7):
+        pivot_index = self.notes.index(note_pivot)
+        note_index = random.randint(max(0, pivot_index - note_radius), min(pivot_index + note_radius, len(self.notes)-1))
+        return self.notes[note_index]
+        
+def main():
+    a = Key('B', 'minor')
+    print a.notes
+    
+if __name__ == '__main__' : main()
